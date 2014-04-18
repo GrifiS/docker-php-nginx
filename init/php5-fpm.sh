372d503f6f2b822926f5cfb7804a6b9a2c006e56
Brad Feehan <git@bradfeehan.com>
2014-04-18 13:25:53 +1000

    Set cgi.fix_pathinfo=0 in PHP-FPM's php.ini

--- init/php5-fpm.sh
+++ init/php5-fpm.sh
@@ -5,5 +5,6 @@ mkdir /etc/service/php5-fpm
 cp /docker-build/services/php5-fpm /etc/service/php5-fpm/run
 
 # Configure php5-fpm
+cp /docker-build/config/php5-fpm/php.ini /etc/php5/fpm/php.ini
 cp /docker-build/config/php5-fpm/php-fpm.conf /etc/php5/fpm/php-fpm.conf
 cp /docker-build/config/php5-fpm/pool.d/www.conf /etc/php5/fpm/pool.d/www.conf
